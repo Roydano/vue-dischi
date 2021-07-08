d0b066f877f02764edde8b4fe2e6e9447188bafd
Roydano <roy.dano90@gmail.com>
2021-07-08 14:34:17 +0200

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="d-flex align-items-center ps-3">
+      <img src="@/assets/spotify-logo.png" alt="spotify logo">
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+
+    header{
+        height: 70px;
+        background-color: #2e3a46;
+
+        img{
+            height: 60%;
+        }
+    }
+    
+</style>
